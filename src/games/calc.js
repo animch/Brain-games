c67e42e24a87a1da95c9c5ad2f25ffc664e4f6cf
Alexander Nimchuk <nimchuk996@gmail.com>
2022-10-22 01:11:08 +1000

    add calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,29 @@
+import { getGameEngine, getRandomArrayElement, getRandomNumber } from '../index.js';
+
+const specification = 'What is the result of the expression?';
+
+const getInstruction = (number1, number2, instruction) => {
+  if (instruction === '+') {
+    return number1 + number2;
+  } if (instruction === '-') {
+    return number1 - number2;
+  } if (instruction === '*') {
+    return number1 * number2;
+  }
+};
+
+const getTask = () => {
+  const number1 = getRandomNumber(1, 50);
+  const number2 = getRandomNumber(1, 50);
+  const operators = ['+', '-', '*'];
+  const instruction = getRandomArrayElement(operators);
+  const gameQuestion = String(`${number1}${instruction}${number2}`);
+  const correctAnswer = String(getInstruction(number1, number2, instruction));
+  return [gameQuestion, correctAnswer];
+};
+
+const getBrainCalc = () => {
+  getGameEngine(specification, getTask);
+};
+
+export default getBrainCalc;
