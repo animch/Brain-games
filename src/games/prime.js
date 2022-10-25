966526f3869803f8fda5e08bf36ec5d47ad2ee03
Alexander Nimchuk <nimchuk996@gmail.com>
2022-10-26 03:56:18 +1000

    add prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,18 @@
+import { getGameEngine, getRandomNumber } from '../index.js';
+
+const specification = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => (number < 2) || (number % 2 !== 0);
+
+const getTask = () => {
+  const number = getRandomNumber(1, 100);
+  const gameQuestion = String(`${number}`);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return [gameQuestion, correctAnswer];
+};
+
+const getBrainPrime = () => {
+  getGameEngine(specification, getTask);
+};
+
+export default getBrainPrime;
