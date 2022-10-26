ace9b9275f2f8a33c0f1dbdd6e1daf4bbea01431
Alexander Nimchuk <nimchuk996@gmail.com>
2022-10-26 17:49:51 +1000

    fixed bugs

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,12 +2,19 @@ import { getGameEngine, getRandomNumber } from '../index.js';
 
 const specification = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (number) => (number === 2) || (number % 2 !== 0);
+const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const getTask = () => {
   const number = getRandomNumber(1, 100);
   const gameQuestion = String(`${number}`);
-  const correctAnswer = String(isPrime(number) ? 'yes' : 'no').toLowerCase();
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
   return [gameQuestion, correctAnswer];
 };
 
