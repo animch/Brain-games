de4de384c5da7c757965b664c384ef379ad71c41
Alexander Nimchuk <nimchuk996@gmail.com>
2022-11-01 18:19:30 +0600

    renaming

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import getUserName from './cli.js';
 
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-export const getRandomArrayElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
-export const getGameEngine = (specification, getTask) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(specification);
+export default (gameRule, getGameTask) => {
+  const userName = getUserName();
+  console.log(gameRule);
 
   const roundCount = 3;
 
   for (let i = 0; i < roundCount; i += 1) {
-    const [gameQuestion, correctAnswer] = getTask();
+    const [gameQuestion, correctAnswer] = getGameTask();
     console.log(`Question: ${gameQuestion}`);
-    const yourAnswer = readlineSync.question('Your answer: ');
-    if (yourAnswer === correctAnswer) {
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
