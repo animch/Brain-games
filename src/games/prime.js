803f1781b13e4eeca7bd40f8d80d4a1cbcdfdaca
Alexander Nimchuk <nimchuk996@gmail.com>
2022-11-04 18:22:54 +0600

    fixed bugs

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,6 +7,9 @@ const minNumber = 1;
 const maxNumber = 100;
 
 const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
